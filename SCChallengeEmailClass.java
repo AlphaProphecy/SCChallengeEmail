ab80e7b64fd2d223dfdad1357261a5c95af6822c
AlphaProphecy <srowbotham@me.com>
2018-10-11 23:24:58 +0100

    Update SCChallengeEmailClass.java
    Completes base challenge

---
+++ SCChallengeEmailClass.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.lang.NullPointerException;
+
+public class SCChallengeEmailClass {
+	public static void main(String[] args)throws MalformedURLException, IOException {
+		System.out.println("1: Name from username");
+		int choice = Integer.parseInt(textInput("Enter your choice:"));
+		switch (choice){
+			case 1:	fullName();
+					break;
+		}
+	}
+	
+	
+	public static String textInput(String request) throws IOException{
+		System.out.println(request);
+		InputStreamReader in = new InputStreamReader(System.in);
+		BufferedReader ir = new BufferedReader(in);
+		String temp = ir.readLine();
+		return temp;
+	}
+	
+	public static void fullName() throws MalformedURLException, IOException {
+		try {
+			String link = "https://www.ecs.soton.ac.uk/people/" + textInput("Enter the username: ");
+			URL url = new URL(link);
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+			
+			String line;
+			String intent = "property=\"name\">";
+			
+			while (!(line = br.readLine()).contains(intent));
+			
+			int pos = line.indexOf(intent) + 16;
+			while (!line.substring(pos, pos+1).contains("<")) {
+				System.out.print(line.substring(pos, pos+1));
+				pos++;
+			}
+			System.out.println();
+			br.close();
+			
+		}
+		catch (NullPointerException e){
+			System.out.println("The username you entered does not have a page.");
+		}
+	}
+	
+	public static void related() throws MalformedURLException, IOException {
+		
+	}
+}
